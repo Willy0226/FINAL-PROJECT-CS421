ae0381c82ef11a905aa34d7e3d69ddd108e4e4e8
BuildTools <unconfigured@null.spigotmc.org>
2021-12-16 16:47:58 -0800

    Renammed function

--- parser.cpp
+++ parser.cpp
@@ -33,35 +33,36 @@ tokentype saved_token = EOFM;
 
 bool token_available = false;
 
-string saved_E_word;//this is the saved english word
+string saved_E_word; //this is the saved english word
 
-unordered_map<string,string> lexicon;//to store the definitions of japanese to english from lexcion.txt
+unordered_map<string, string> lexicon; //to store the definitions of japanese to english from lexcion.txt
 
-ofstream file("traslated.txt");//file where will send our output
+ofstream file("traslated.txt"); //file where will send our output
 //ofstream translation= "translation.txt";
 // ----- Four Utility Functions and Globals -----------------------------------
 
 // ** Need syntaxerror1 and syntaxerror2 functions (each takes 2 args)
 //    to display syntax error messages as specified by me.
 
-
 //PURPOSE:to store the definitions from the file lexicon.txt
 //PARAMETER: map lexicon will be used as a hashed map in order to store the values
-void translations(){
-  ifstream fin ("lexicon.txt", ios::in);
+void dictionary()
+{
+  ifstream fin("lexicon.txt", ios::in);
   string JAPword;
   string ENGword;
-  while(fin>>JAPword)
-    {
-      fin>>ENGword;
-      lexicon[JAPword]=ENGword;}
+  while (fin >> JAPword)
+  {
+    fin >> ENGword;
+    lexicon[JAPword] = ENGword;
+  }
 
-    unordered_map<string, string>:: iterator itr;
-    /*to print the map
+  unordered_map<string, string>::iterator itr;
+  /*to print the map
     for (itr = lexicon.begin(); itr != lexicon.end(); itr++)
         {
         cout << itr->first << "\t" << itr->second << endl;
-        }*/    
+        }*/
 }
 
 //this will generate the english word from save_E_word or
@@ -70,26 +71,39 @@ void translations(){
 //PARAMETER: saved_Eword a string and saved_toke of type tokentype
 void gen(string line_type)
 {
-  if(line_type!="TENSE"){
-    file<<line_type<<" "<<saved_E_word<<endl;
-    }
-  else{file<<line_type<<" "<<tokenName[saved_token]<<endl<<endl;}
+  if (line_type != "TENSE")
+  {
+    file << line_type << " " << saved_E_word << endl;
+  }
+  else
+  {
+    file << line_type << " " << tokenName[saved_token] << endl
+         << endl;
+  }
 }
 //just to get the english word to the map we haved created from lexicon
 //if the map does not march we use the saved_lexeme
-void getEword(){
-  
-  unordered_map<string, string>:: iterator itr;
-  itr=lexicon.find(saved_lexeme);
+void getEword()
+{
+
+  unordered_map<string, string>::iterator itr;
+  itr = lexicon.find(saved_lexeme);
   //
-  if(itr != lexicon.end()) { saved_E_word = itr->second; } else {saved_E_word = saved_lexeme;}   
+  if (itr != lexicon.end())
+  {
+    saved_E_word = itr->second;
+  }
+  else
+  {
+    saved_E_word = saved_lexeme;
+  }
 }
 
 // Type of error: Bad token
 // Done by: Issac Ferrel
 void syntaxerror1(tokentype expected, string save_lexeme)
 {
-  cout << "SYNTAX ERROR: expected " <<tokenName[expected] << ", but found " << save_lexeme << endl;
+  cout << "SYNTAX ERROR: expected " << tokenName[expected] << ", but found " << save_lexeme << endl;
   /*ofstream errors;
   errors.open("errors.txt");
   errors << "SYNTAX ERROR: expected " <<tokenName[expected] << ", but found " << save_lexeme << endl;
@@ -111,8 +125,8 @@ void syntaxerror1(tokentype expected, string save_lexeme)
 	}
 	}*/
   // else{
-exit(1);
-//} // exit
+  exit(1);
+  //} // exit
 }
 
 // Type of error: Not the correct word
@@ -131,13 +145,14 @@ void syntaxerror2(string save_lexeme, string parseFunction)
 tokentype next_token()
 {
   if (!token_available)
-    {
-      //cout << "BEFORE: " << tokenName[saved_token] << endl;
-      scanner(saved_token, saved_lexeme);
-       cout << "Scanner called using word " << "\"" + saved_lexeme + "\"" << endl;
-      //cout << "AFTER: " << tokenName[saved_token] << endl;
-      token_available = true;
-    }
+  {
+    //cout << "BEFORE: " << tokenName[saved_token] << endl;
+    scanner(saved_token, saved_lexeme);
+    cout << "Scanner called using word "
+         << "\"" + saved_lexeme + "\"" << endl;
+    //cout << "AFTER: " << tokenName[saved_token] << endl;
+    token_available = true;
+  }
   return saved_token;
 }
 
@@ -146,16 +161,16 @@ tokentype next_token()
 bool match(tokentype expected)
 {
   if (next_token() != expected)
-    {
-      syntaxerror1(expected, saved_lexeme);
-      return false;
-    }
+  {
+    syntaxerror1(expected, saved_lexeme);
+    return false;
+  }
   else
-    {
-      cout << "matched " << tokenName[expected] << endl;
-      token_available = false;
-      return true;
-    }
+  {
+    cout << "matched " << tokenName[expected] << endl;
+    token_available = false;
+    return true;
+  }
 }
 
 /// ----- RDP functions - one per non-term -------------------
@@ -170,18 +185,18 @@ void story()
 {
   s();
   while (true)
+  {
+    switch (next_token())
     {
-      switch (next_token())
-	{
-	case CONNECTOR:
-	case WORD1:
-	case PRONOUN:
-	  s();
-	  break;
-	default:
-	  return;
-	}
+    case CONNECTOR:
+    case WORD1:
+    case PRONOUN:
+      s();
+      break;
+    default:
+      return;
     }
+  }
 }
 // Grammar:<s> ::= [CONNECTOR] <noun> SUBJECT <after subject>
 // Grammar updated:<s>::=[CONNECTOR #getEword# #gen(CONNECTOR)#]<noun> #getEword# SUBJECT #gen(ACTOR)# <after subject>
@@ -189,11 +204,11 @@ void story()
 void s()
 {
   if (next_token() == CONNECTOR)
-    {
-      match(CONNECTOR);
-      getEword();
-      gen("CONNECTOR");
-    }
+  {
+    match(CONNECTOR);
+    getEword();
+    gen("CONNECTOR");
+  }
   noun();
   getEword();
   match(SUBJECT);
@@ -207,59 +222,59 @@ void s()
 void after_subject()
 {
   switch (next_token())
-    {
-    case WORD2:
-      verb();
-      getEword();
-      gen("ACTION");
-      tense();
-      gen("TENSE");
-      match(PERIOD);
-      break;
-    case WORD1:
-    case PRONOUN:
-      noun();
-      getEword();
-      after_noun();
-      break;
-    default:
-      syntaxerror2(saved_lexeme, "after_subject");
-    }
+  {
+  case WORD2:
+    verb();
+    getEword();
+    gen("ACTION");
+    tense();
+    gen("TENSE");
+    match(PERIOD);
+    break;
+  case WORD1:
+  case PRONOUN:
+    noun();
+    getEword();
+    after_noun();
+    break;
+  default:
+    syntaxerror2(saved_lexeme, "after_subject");
+  }
 }
 // Grammar:<after noun> ::= <be> PERIOD|DESTINATION <verb><tense>PERIOD|OBJECT<after object>
 // Grammar updated:<after noun> ::= <be>#gen(DESCRIPTION)##gen(TENSE)# PERIOD|
-//DESTINATION #gen(TO)# <verb> #getEword# #gen(ACTION)# <tense> #gen(TENSE) PERIOD| 
+//DESTINATION #gen(TO)# <verb> #getEword# #gen(ACTION)# <tense> #gen(TENSE) PERIOD|
 //OBJECT #gen(Object)# <after object>
 // Done by: Ting Wei Chien(Willy)
 void after_noun()
 {
   switch (next_token())
-    {
-    case IS:
-    case WAS:
-      be();
-      gen("DESCRIPTION");
-      gen("TENSE");
-      match(PERIOD);
-      break;
-    case DESTINATION:
-      match(DESTINATION);
-      gen("TO");
-      verb();
-      getEword();
-      gen("ACTION");
-      tense();
-      gen("TENSE");
-      match(PERIOD);
-      break;
-    case OBJECT:
-      match(OBJECT);
-      gen("OBJECT");
-      after_object();
-      break;
-    default:
-      syntaxerror2(saved_lexeme, "after_noun");
-    }
+  {
+  case IS:
+  case WAS:
+    be();
+    gen("DESCRIPTION");
+    gen("TENSE");
+    match(PERIOD);
+    break;
+  case DESTINATION:
+    match(DESTINATION);
+    gen("TO");
+    verb();
+    getEword();
+    gen("ACTION");
+    tense();
+    gen("TENSE");
+    match(PERIOD);
+    break;
+  case OBJECT:
+    match(OBJECT);
+    gen("OBJECT");
+    after_object();
+    break;
+  default:
+    syntaxerror2(saved_lexeme, "after_noun");
+  }
 }
 // Grammar:<afterobject> ::=<verb><tense>PERIOD|<noun>DESTINATION<verb><tense>PERIOD
 // Grammar updated:<afterobject> ::=<verb> #getEword# #gen(ACTION)# <tense> #gen(TENSE)# PERIOD|
@@ -268,47 +283,47 @@ void after_noun()
 void after_object()
 {
   switch (next_token())
-    {
-    case WORD2:
-      verb();
-      getEword();
-      gen("ACTION");
-      tense();
-      gen("TENSE");
-      match(PERIOD);
-      break;
-    case WORD1:
-    case PRONOUN:
-      noun();
-      getEword();
-      match(DESTINATION);
-      gen("TO");
-      verb();
-      getEword();
-      gen("ACTION");
-      tense();
-      gen("TENSE");
-      match(PERIOD);
-      break;
-    default:
-      syntaxerror2(saved_lexeme, "after_object");
-    }
+  {
+  case WORD2:
+    verb();
+    getEword();
+    gen("ACTION");
+    tense();
+    gen("TENSE");
+    match(PERIOD);
+    break;
+  case WORD1:
+  case PRONOUN:
+    noun();
+    getEword();
+    match(DESTINATION);
+    gen("TO");
+    verb();
+    getEword();
+    gen("ACTION");
+    tense();
+    gen("TENSE");
+    match(PERIOD);
+    break;
+  default:
+    syntaxerror2(saved_lexeme, "after_object");
+  }
 }
 // Grammar:<noun> ::= WORD1 | PRONOUN
 // Done by: Ting Wei Chien(Willy)
 void noun()
 {
   switch (next_token())
-    {
-    case WORD1:
-      match(WORD1);
-      break;
-    case PRONOUN:
-      match(PRONOUN);
-      break;
-    default:
-      syntaxerror2(saved_lexeme, "noun");
-    }
+  {
+  case WORD1:
+    match(WORD1);
+    break;
+  case PRONOUN:
+    match(PRONOUN);
+    break;
+  default:
+    syntaxerror2(saved_lexeme, "noun");
+  }
 }
 // Grammar:<verb> ::= WORD2
 // Done by: Issac Ferrel
@@ -321,38 +336,38 @@ void verb()
 void be()
 {
   switch (next_token())
-    {
-    case IS:
-      match(IS);
-      break;
-    case WAS:
-      match(WAS);
-      break;
-    default:
-      syntaxerror2(saved_lexeme, "be");
-    }
+  {
+  case IS:
+    match(IS);
+    break;
+  case WAS:
+    match(WAS);
+    break;
+  default:
+    syntaxerror2(saved_lexeme, "be");
+  }
 }
 // Grammar:<tense> := VERBPAST  | VERBPASTNEG | VERB | VERBNEG
 // Done by: Christian Tobin
 void tense()
 {
   switch (next_token())
-    {
-    case VERBPAST:
-      match(VERBPAST);
-      break;
-    case VERBPASTNEG:
-      match(VERBPASTNEG);
-      break;
-    case VERB:
-      match(VERB);
-      break;
-    case VERBNEG:
-      match(VERBNEG);
-      break;
-    default:
-      syntaxerror2(saved_lexeme, "tense");
-    }
+  {
+  case VERBPAST:
+    match(VERBPAST);
+    break;
+  case VERBPASTNEG:
+    match(VERBPASTNEG);
+    break;
+  case VERB:
+    match(VERB);
+    break;
+  case VERBNEG:
+    match(VERBNEG);
+    break;
+  default:
+    syntaxerror2(saved_lexeme, "tense");
+  }
 }
 
 //string filename;
--- translator.cpp
+++ translator.cpp
@@ -1,6 +1,6 @@
-#include<iostream>
-#include<fstream>
-#include<string>
+#include <iostream>
+#include <fstream>
+#include <string>
 #include "parser.cpp"
 using namespace std;
 
@@ -18,13 +18,12 @@ using namespace std;
 
 // ** Declare Lexicon (i.e. dictionary) that will hold the content of lexicon.txt
 // Make sure it is easy and fast to look up the translation.
-// Do not change the format or content of lexicon.txt 
+// Do not change the format or content of lexicon.txt
 //  Done by: Issac Ferrel
-
 
 // ** Additions to parser.cpp here:
 //    getEword() - using the current saved_lexeme, look up the English word
-//                 in Lexicon if it is there -- save the result   
+//                 in Lexicon if it is there -- save the result
 //                 in saved_E_word
 //  Done by: Issac Ferrel
 //    gen(line_type) - using the line type,
@@ -34,13 +33,12 @@ using namespace std;
 
 // ----- Changes to the parser.cpp content ---------------------
 
-// ** Comment update: Be sure to put the corresponding grammar 
+// ** Comment update: Be sure to put the corresponding grammar
 //    rule with semantic routine calls
-//    above each non-terminal function 
+//    above each non-terminal function
 
 // ** Each non-terminal function should be calling
 //    getEword and/or gen now.
-
 
 // ---------------- Driver ---------------------------
 
@@ -51,26 +49,25 @@ int main()
   //necessary do not delete look at setup_table for details.
   setup_table();
   //** opens the lexicon.txt file and reads it into Lexicon
-  translations();
-  //** closes lexicon.txt 
+  dictionary();
+  //** closes lexicon.txt
   //** opens the output file translated.txt
-  
+
   cout << "Enter the input file name: ";
   string filename;
   cin >> filename;
   fin.open(filename.c_str());
-  
+
   //** calls the <story> to start parsing
   //** calls the <story> to start parsing
   story();
   cout << "Successfully parsed <story>." << endl;
   //** closes the input file
   fin.close();
-  //** closes the input file 
+  //** closes the input file
   //** closes traslated.txt
- 
-}// end
+
+} // end
 //** require no other input files!
 //** syntax error EC requires producing errors.txt of error messages
 //** tracing On/Off EC requires sending a flag to trace message output functions
-
